6d705fc5d3a09f00a800151d8671f533da4ff897
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-10 14:09:11 +0530

    [Umesh]Added: simple linked list

--- src/test/java/com/bridgelabz/datastructure/MyNodeTest.java
+++ src/test/java/com/bridgelabz/datastructure/MyNodeTest.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.datastructure;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MyNodeTest {
 	
+	@Test
+	public void Given3Numbers_WhenLinked_ShouldPassLinkedListTest(){
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+						 mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+		}
 }
