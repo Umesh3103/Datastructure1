6c65326c2900d49be22a6d77b534523a3f89ca60
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-12 19:52:04 +0530

    [Umesh]Added: finding frequency of a word in a large paragraph

---
+++ src/test/java/com/bridgelabz/datastructure/MyLinkedHashMapTest.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.datastructure;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MyLinkedHashMapTest {
+	
+	@Test
+	public void d(){
+		String sentence = "Paranoids are not paranoid beacuse they are paranoid but"+
+						" bcz they keep putting themselves into "+
+						"paranoid avoidable situations";
+		MyLinkedHashMap<String,Integer> myLinkedHashMap= new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word: words){
+			Integer value = myLinkedHashMap.get(word);
+			if(value==null){
+				value=1;
+			}
+			else{
+				value = value + 1;
+			}
+			myLinkedHashMap.add(word,value);
+		}	
+		int frequency=myLinkedHashMap.get("paranoid");
+		System.out.println(myLinkedHashMap);
+		Assert.assertEquals(3, frequency);
+	}
+}
